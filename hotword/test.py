84fa8e31c3572ee5b6a0c86a7a57874ddf825ce1
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-04 01:26:26 +0700

    Add hotword

---
+++ hotword/test.py
@@ -0,0 +1,18 @@
+import time
+from precise_runner import PreciseEngine, PreciseRunner
+
+print('start')
+def on_prediction(prob):
+    print('!' if prob > 0.5 else '.', end='', flush=True)
+
+def on_activation():
+    print('Activation')
+
+engine = PreciseEngine('C:\\Users\\TC\\Anaconda3\\Scripts\\precise-engine.exe', 'jao-sandy.pb') 
+# PreciseEngine(ที่ตั้งโฟลเดอร์ Scripts ของ precise-engine ,  ไฟล์ model)
+# หากรันบน Linux ใช้ precise-engine/precise-engine ใน precise-engine
+runner = PreciseRunner(engine, on_prediction=on_prediction, on_activation=on_activation, trigger_level=0)
+runner.start()
+
+while 1:
+    time.sleep(1)
