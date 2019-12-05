8854ed59f002ab457c6431a65df5831590fe696d
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-05 23:35:58 +0700

    ๊Update code

--- run.py
+++ run.py
@@ -4,23 +4,28 @@ from precise_runner import PreciseEngine, PreciseRunner
 import speech_recognition as sr
 from tts import TTS
 from vad import run
+import sys
 print('start')
 t=TTS()
 r = sr.Recognizer()
+t.listen("NSC 2020 แสนดีผู้ช่วยผู้สูงอายุ กำลังเริ่มต้นการทำงาน")
 def on_prediction(prob:float)->None:
     print(print(prob) if prob > 0.5 else '.', end='', flush=True)
 
 """def on_activation():
     print('Activation')
     winsound.PlaySound("*", winsound.MB_OK)"""
-
+stauts=""
 def process(text:str)->str:
     if "เวลา" in text:
         text=now()
+    elif "ออก" in text or "ลาก่อน" in text:
+        text="ลาก่อนค่ะ"
     else:
         text="คุณพูดว่า "+text
     return text
 def on_activation():
+    global stauts
     global t
     t.listen("หวัดดีค่ะ")
     global r
@@ -34,7 +39,10 @@ def on_activation():
         print("กำลังรอเสียง")
         text=r.recognize_google(audio,language = "th-TH")
         print(text)
-        t.listen(process(text))
+        tt=process(text)
+        t.listen(tt)
+        if tt=="ลาก่อนค่ะ":
+            stauts="exit"#sys.exit(0)
     except sr.RequestError as e:
         print(e)
     except Exception as e:
@@ -45,6 +53,7 @@ engine = PreciseEngine('C:\\Users\\TC\\Anaconda3\\Scripts\\precise-engine.exe', 
 # หากรันบน Linux ใช้ precise-engine/precise-engine ใน precise-engine
 runner = PreciseRunner(engine, on_prediction=on_prediction, on_activation=on_activation, trigger_level=0)
 runner.start()
-
 while 1:
     time.sleep(1)
+    if stauts=="exit":
+        break
