83a158e7ec03ee04099062c77dd6a2b614caf599
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-09 02:03:51 +0700

    Update thaipbs.py

--- news/thaipbs.py
+++ news/thaipbs.py
@@ -7,37 +7,64 @@ class news(object):
     def get_news(self):
         return self.data.entries
 class politics(news):
+    """
+    ข่าวการเมือง
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/politics")
 
 class social(news):
+    """
+    ข่าวสังคม
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/social")
 
 class crime(news):
+    """
+    ข่าวอาชญากรรม
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/crime")
 
 class region(news):
+    """
+    ข่าวภูมิภาค
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/region")
 
 class environment(news):
+    """
+    ข่าวสิ่งแวดล้อม
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/environment")
 
 class economy(news):
+    """
+    ข่าวเศรษฐกิจ
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/economy")
 
 class foreign(news):
+    """
+    ข่าวต่างประเทศ
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/foreign")
 
 class sport(news):
+    """
+    ข่าวกีฬา
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/news/sport")
 
 class breakingnews(news):
+    """
+    ข่าวเด่น
+    """
     def __init__(self):
         super().__init__("https://news.thaipbs.or.th/rss/breakingnews")
