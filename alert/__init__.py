222257272b8f511cebb5aabe3a1f6e7e6da8d187
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-15 01:56:45 +0700

    add alert

---
+++ alert/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from tinydb import TinyDB, Query
+from pythainlp.util import thai_day2datetime
+import pytz
+import datetime as dt
+import pytz
+db = TinyDB('./db.json')
+timezone = pytz.timezone('Asia/Bangkok')
+def add(date:str,time:str,text:str)->None:
+    global db,timezone
+    t= time.split(":")
+    hour = int(t[0])
+    minute = int(t[1])
+    d = timezone.localize(thai_day2datetime(date)).replace(hour=hour, minute=minute, second=0, microsecond=0)
+    if d == None: pass
+    db.insert({'date': str(d),'text':text})
+    print(d)
+
+if __name__ == "__main__":
+    date = input("วัน : ")
+    t = input("เวลา (1:10) :")
+    text = input("ข้อความ : ")
+    add(date, t, text)
+    print("เพิ่มเรียบร้อย\n\n")
+    print()
---
+++ alert/run.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from tinydb import TinyDB, Query
+import pytz
+import datetime as dt
+import time
+db = TinyDB('./db.json')
+N = Query()
+timezone = pytz.timezone('Asia/Bangkok')
+tt = ''
+while True:
+    o=str(timezone.localize(dt.datetime.now()).replace(second=0, microsecond=0))
+    t=db.search(N.date == o)
+    if t!=[] and tt!=o:
+        print(t[0]['text'])
+        tt=o
