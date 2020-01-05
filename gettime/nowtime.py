12d1c85c82e8dd53c621decdb923ad7873805b3e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-05 17:32:50 +0700

    Update nowtime.py

--- gettime/nowtime.py
+++ gettime/nowtime.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from pythainlp.util import thai_time
+from pythainlp.util import thai_time,thai_strftime
 
 def now(text):
+    _n=datetime.now()
     if "วัน" and "เวลา":
-        pass
+        text = "วันนี้"+thai_strftime(_n, "%A %d %B %Y ")+"เวลา "+thai_time(str(_n.strftime('%H:%M')),"6h")+"ค่ะ"
+    elif "วัน" in text:
+        text = "วันนี้"+thai_strftime(_n, "%A %d %B %Y ")+"ค่ะ"
     elif 'เวลา' in text or 'เพลา' in text or 'โมง' in text:
-        text = "ขณะนี้เวลา "+thai_time(str(datetime.now().strftime('%H:%M')),"6h")+"ค่ะ"#นาฬิกา
+        text = "ขณะนี้เวลา "+thai_time(str(_n.strftime('%H:%M')),"6h")+"ค่ะ"#นาฬิกา
     else:
         text = "ระบบบอกเวลา ยังไม่รองรับคำสั่งนี้ค่ะ"
     return text
