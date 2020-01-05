7b6dac13cfaa72bcf98a6c9b8c34d139dcd04da5
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-06 02:10:07 +0700

    update game

--- nlu/classification/train.py
+++ nlu/classification/train.py
@@ -4,8 +4,20 @@ from sklearn.model_selection import train_test_split
 file = "../dataset/data-nottag.set"
 import pandas as pd
 colnames=['text', 'tag']
+
+
+from pythainlp.tokenize import word_tokenize,Trie
+from pythainlp.corpus import thai_stopwords
+# ...
+#filtered_words = [word for word in word_list if word not in list(thai_stopwords())]
+o= Trie(list(thai_stopwords()))
+def filtered_words(x:tuple):
+    #w = .lower()#word_tokenize(x[0],custom_dict=o)
+    #ww = [word for word in w if word not in list(thai_stopwords())]
+    return (x[0].lower().strip() ,x[1])#(''.join(ww),x[1])
+
 user1 = pd.read_csv(file, names=colnames, header=None,sep="|")
-data= [tuple(x) for x in user1.to_records(index=False)]
+data= [filtered_words(tuple(x)) for x in user1.to_records(index=False)]
 def features(text):
     wordlist = word_tokenize(text)
     f={}
