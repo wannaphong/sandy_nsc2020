968a70c52a369f6b3327331c941ccfdba5324528
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-10 00:01:41 +0700

    Create key.py

---
+++ keyboard/key.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 23:55:23 2020
+
+@author: TC
+"""
+
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
