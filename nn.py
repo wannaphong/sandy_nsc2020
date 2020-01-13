159f40024c0f8b3adada3fe61639544f7be6dc32
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-14 00:51:39 +0700

    Update nn.py

--- nn.py
+++ nn.py
@@ -7,6 +7,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, Tf
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+model = MLPClassifier(solver='adam', alpha=1e-5,hidden_layer_sizes=(24, 6), verbose=False, max_iter=2000)
 nb = Pipeline([('vect', CountVectorizer(tokenizer=process_thai, ngram_range=(1,2))),
                ('tfidf', TfidfTransformer()),
                ('chi2', SelectKBest(chi2, k='all')),
