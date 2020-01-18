3eca40c464efd9b8fd28163b92b962c4dd7ba46a
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-18 22:32:43 +0700

    Update run.py

--- alert/run.py
+++ alert/run.py
@@ -5,15 +5,19 @@ import pytz
 import datetime as dt
 import time
 tts=TTS()
-db = TinyDB('./db.json')
-N = Query()
-timezone = pytz.timezone('Asia/Bangkok')
-tt = ''
-while True:
-    o=str(timezone.localize(dt.datetime.now()).replace(second=0, microsecond=0))
-    t=db.search((N.date == o) & (N.alert == True))#db.search(N.date == o)
-    if t!=[] and tt!=o:
-        a="การแจ้งเตือน มีการแจ้งเตือนว่า"+t[0]['text']
-        tts.listen(a)
-        print(a)
-        tt=o
+def alert_run():
+    db = TinyDB('./db.json')
+    N = Query()
+    timezone = pytz.timezone('Asia/Bangkok')
+    tt = ''
+    while True:
+        o=str(timezone.localize(dt.datetime.now()).replace(second=0, microsecond=0))
+        t=db.search((N.date == o) & (N.alert == True))#db.search(N.date == o)
+        if t!=[] and tt!=o:
+            a="การแจ้งเตือน มีการแจ้งเตือนว่า"+t[0]['text']
+            tts.listen(a)
+            print(a)
+            tt=o
+
+if __name__ == '__main__':
+    alert_run()
